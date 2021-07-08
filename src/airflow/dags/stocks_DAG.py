272cb1fdc8020aee2c8f752da64010e01615903a
mark <mcgaritym@gmail.com>
2021-07-08 18:58:58 -0400

    updated query

---
+++ src/airflow/dags/stocks_DAG.py
@@ -0,0 +1,82 @@
+# import libraries
+from datetime import timedelta
+import logging
+import stocks_extract_s3, stocks_extract_rds, stocks_transform_rds, stocks_load_query
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+# specify arguments for DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['mcgaritym@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG(
+    'stock_screener',
+    default_args=default_args,
+    description='stock_screener DAG test',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(2),
+    tags=['example'],
+)
+
+
+# stocks_extract_s3 = BashOperator(
+#     task_id='stocks_extract_s3',
+#     bash_command='python 02_stocks_extract_s3.py',
+#     dag=dag
+# )
+#
+# stocks_extract_rds = BashOperator(
+#     task_id='stocks_extract_rds',
+#     bash_command='python 02_stocks_extract_rds.py',
+#     dag=dag
+# )
+#
+# stocks_transform_rds = BashOperator(
+#     task_id='stocks_transform_rds',
+#     bash_command='python 02_stocks_transform_rds.py',
+#     dag=dag
+# )
+#
+#
+# stocks_load_query = BashOperator(
+#     task_id='stocks_load_query',
+#     bash_command='python 02_stocks_load_query.py',
+#     dag=dag
+# )
+
+stocks_extract_s3 = PythonOperator(
+    task_id='stocks_extract_s3',
+    python_callable=stocks_extract_s3.main,
+    dag=dag
+)
+
+stocks_extract_rds = PythonOperator(
+    task_id='stocks_extract_rds',
+    python_callable=stocks_extract_rds.main,
+    dag=dag
+)
+
+stocks_transform_rds = PythonOperator(
+    task_id='stocks_transform_rds',
+    python_callable=stocks_transform_rds.main,
+    dag=dag
+)
+
+
+stocks_load_query = PythonOperator(
+    task_id='stocks_load_query',
+    python_callable=stocks_load_query.main,
+    dag=dag
+)
+
+
+stocks_extract_s3 >> stocks_extract_rds >> stocks_transform_rds >> stocks_load_query
---
+++ src/airflow/dags/stocks_extract_s3.py
@@ -0,0 +1,117 @@
+# import libraries
+import pandas as pd
+from src.setup.setup import Setup
+from src.setup import config
+from io import BytesIO, StringIO
+
+# call Setup class as connection
+connection = Setup(config.user, config.pwd, config.host, config.port, 'stocks',
+                   config.service_name, config.region_name, config.aws_access_key_id,
+                   config.aws_secret_access_key, config.local_host, config.local_user,
+                   config.local_pwd, config.local_port, 'Stocks')
+
+# create AWS s3 connection
+s3_client = connection.s3_client()
+s3_resource = connection.s3_resource()
+
+# create local mySQL connection
+connection.local_database()
+my_sql = connection.local_connect()
+
+def empty_s3(bucket_name):
+
+    # specify bucket
+    bucket = s3_resource.Bucket(bucket_name)
+
+    # delete all
+    bucket.objects.all().delete()
+
+# collect stock financials in local directory and upload to s3
+def upload_financials_s3(sql_table, bucket_name, object_name):
+
+    # connect to local SQL
+    df_financials = pd.read_sql(sql="SELECT * FROM {};".format(sql_table), con=my_sql)
+    print(df_financials.head())
+
+    # convert to csv in memory
+    csv_buffer = StringIO()
+    df_financials.to_csv(csv_buffer)
+
+    try:
+
+        # upload to s3
+        # s3 = connection.s3()
+        s3_resource.Object(bucket_name, object_name).put(Body=csv_buffer.getvalue())
+        print('{} uploaded to s3 successfully'.format(object_name))
+
+    except:
+        # error
+        print('Error: Did not upload {} to s3'.format(object_name))
+
+# collect stock tickers in local SQL and upload to s3
+def upload_tickers_s3(sql_table, bucket_name, object_name):
+
+    # connect to local SQL
+    df_financials = pd.read_sql(sql="SELECT * FROM {};".format(sql_table), con=my_sql)
+    print(df_financials.head())
+
+    # convert to csv in memory
+    csv_buffer = StringIO()
+    df_financials.to_csv(csv_buffer)
+
+    # upload to s3 as csv
+    try:
+        # upload to s3
+        # s3 = connection.s3()
+        s3_resource.Object(bucket_name, object_name).put(Body=csv_buffer.getvalue())
+        print('{} uploaded to s3 successfully'.format(object_name))
+
+    except:
+        # error
+        print('Error: Did not upload {} to s3'.format(object_name))
+
+
+# collect ride files in local directory and upload to s3
+def upload_sentiment_s3(sql_table, bucket_name, object_name):
+
+    # connect to local SQL
+    df_sentiment = pd.read_sql(sql="SELECT * FROM {};".format(sql_table), con=my_sql)
+    print(df_sentiment.head())
+
+    # convert to parquet in memory
+    parquet_buffer = BytesIO()
+    df_sentiment.to_parquet(parquet_buffer)
+
+    # upload to s3 as parquet
+    try:
+        # upload to s3
+        # s3 = connection.s3()
+        s3_resource.Object(bucket_name, object_name).put(Body=parquet_buffer.getvalue())
+        print('{} uploaded to s3 successfully'.format(object_name))
+
+    except:
+        # error
+        print('Error: Did not upload {} to s3'.format(object_name))
+
+
+def main():
+    # call functions
+    empty_s3("stocks.bucket")
+    upload_sentiment_s3("news_sentiment", "stocks.bucket", "news_sentiment.parquet")
+    upload_tickers_s3("stock_tickers", "stocks.bucket", "stock_tickers.csv")
+    upload_financials_s3("stock_financials", "stocks.bucket", "stock_financials.csv")
+
+    # print all s3 buckets
+    for bucket in s3_resource.buckets.all():
+        print(bucket.name)
+
+    # # print objects within S3 bucket
+    for object in s3_resource.Bucket('stocks.bucket').objects.all():
+        print(object)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
